56aafe90aeb3fbeacec11dd7c2f16c392d1c881a
Daniel Kopka <daniel.kopka@gmail.com>
2015-01-05 14:07:22 +0100

    cleanup

--- setup.py
+++ setup.py
@@ -1,19 +1,30 @@
-# coding=utf8
-
 from setuptools import setup
+from syncano import __version__
 
-setup(name='syncano',
-      version='0.6.2',
-      description='Python Library for syncano.com api',
-      author=u'Piotr Czesław Kalmus',
-      author_email='piotr.kalmus@syncano.com',
-      url='http://syncano.com',
-      packages=['syncano',],
-      classifiers=['Development Status :: 4 - Beta',
-                   'License :: OSI Approved :: MIT License',
-                   'Programming Language :: Python',
-                   'Programming Language :: Python :: 2.7',
-                   'Programming Language :: Python :: 3.3'],
-      install_requires=['gevent==1.0.1']
-    )
 
+def readme():
+    with open("README.md") as f:
+        return f.read()
+
+setup(
+    name='syncano',
+    version=__version__,
+    description='Python Library for syncano.com api',
+    long_description=readme(),
+    author='Daniel Kopka',
+    author_email='daniel.kopka@syncano.com',
+    url='http://syncano.com',
+    packages=[
+        'syncano',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+    ],
+    install_requires=[
+        'requests==2.5.1',
+    ]
+)
