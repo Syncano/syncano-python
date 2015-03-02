00df09e7a4416687e258e9b5cc37b8dca663499c
Daniel Kopka <daniel.kopka@syncano.com>
2015-03-02 18:42:52 +0100

    [SYNPYTH-51] bettre error message

--- syncano/models/fields.py
+++ syncano/models/fields.py
@@ -118,7 +118,7 @@ class Field(object):
         Returns field's value prepared for usage in HTTP request query.
         """
         if not self.query_allowed:
-            raise self.ValidationError('Queries on this field are not supported.')
+            raise self.ValidationError('Querie on this field is not supported.')
 
         return self.to_native(value)
 
