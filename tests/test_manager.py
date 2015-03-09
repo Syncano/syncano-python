e3165cdd078f0fbfc5732e306fbaed4ab3d974b4
Daniel Kopka <daniel.kopka@syncano.com>
2015-03-09 12:24:04 +0100

    [SYNPYTH-59] draft

---
+++ tests/test_manager.py
@@ -0,0 +1,38 @@
+import unittest
+
+try:
+    from unittest import mock
+except ImportError:
+    import mock
+
+
+class CloneTestCase(unittest.TestCase):
+    pass
+
+
+class ManagerDescriptorTestCase(unittest.TestCase):
+    pass
+
+
+class RelatedManagerDescriptorTestCase(unittest.TestCase):
+    pass
+
+
+class ManagerTestCase(unittest.TestCase):
+    pass
+
+
+class CodeBoxManagerTestCase(unittest.TestCase):
+    pass
+
+
+class WebhookManagerTestCase(unittest.TestCase):
+    pass
+
+
+class ObjectManagerTestCase(unittest.TestCase):
+    pass
+
+
+class SchemaManagerTestCase(unittest.TestCase):
+    pass
