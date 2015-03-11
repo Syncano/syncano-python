bf79c27db4e5e75b1d7a832d2c145a6327d948e0
Daniel Kopka <daniel.kopka@syncano.com>
2015-03-11 17:04:23 +0100

    [SYNPYTH-60] draft

---
+++ tests/test_options.py
@@ -0,0 +1,43 @@
+import unittest
+
+
+class OptionsTestCase(unittest.TestCase):
+
+	def test_meta_inheritance(self):
+		pass
+
+	def test_build_properties(self):
+		pass
+
+	def test_contribute_to_class(self):
+		pass
+
+	def test_resolve_parent_data(self):
+		pass
+
+	def test_add_field(self):
+		pass
+
+	def test_get_field(self):
+		pass
+
+	def test_get_endpoint(self):
+		pass
+
+	def test_get_endpoint_properties(self):
+		pass
+
+	def test_get_endpoint_path(self):
+		pass
+
+	def test_get_endpoint_methods(self):
+		pass
+
+	def test_resolve_endpoint(self):
+		pass
+
+	def test_get_endpoint_query_params(self):
+		pass
+
+	def test_get_path_properties(self):
+		pass
