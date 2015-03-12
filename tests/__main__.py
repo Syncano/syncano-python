493a2c7b705378430e4e215e415ebcea5dd24002
Daniel Kopka <daniel.kopka@syncano.com>
2015-03-12 12:29:43 +0100

    [4.0] proper location for xml files

--- tests/__main__.py
+++ tests/__main__.py
@@ -1,9 +1,11 @@
+import os
 import unittest
 import xmlrunner
 
 
 if __name__ == '__main__':
+    output = os.path.join(os.getenv('CIRCLE_TEST_REPORTS', ''), 'junit')
     suite = unittest.TestLoader().discover('.')
-    runner = xmlrunner.XMLTestRunner(output='reports', verbosity=2,
+    runner = xmlrunner.XMLTestRunner(output=output, verbosity=2,
                                      failfast=False, buffer=False)
     runner.run(suite)
