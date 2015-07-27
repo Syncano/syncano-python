ebf7f1401dfa4f7b8f6bdc7d9cf10c65daeae388
Zhe Brah <zhebrah@gmail.com>
2015-07-27 11:22:29 +0200

    simplify setting default instance

--- syncano/__init__.py
+++ syncano/__init__.py
@@ -69,11 +69,9 @@ def connect(*args, **kwargs):
     from syncano.models import registry
 
     default_connection.open(*args, **kwargs)
-    instance = kwargs.get('instance_name')
+    instance = INSTANCE or kwargs.get('instance_name')
 
-    if INSTANCE:
-        registry.set_default_instance(INSTANCE)
-    elif instance:
+    if instance is not None:
         registry.set_default_instance(instance)
     return registry
 
