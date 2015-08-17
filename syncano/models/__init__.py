679ecde8b819ca29f3e902eceeaf4f99a8f71120
Zhe Brah <zhebrah@gmail.com>
2015-08-17 12:04:23 +0200

    backward compatibility for imports from base

--- syncano/models/__init__.py
+++ syncano/models/__init__.py
@@ -1,9 +1,1 @@
 from .base import *  # NOQA
-from .fields import *  # NOQA
-from .instances import *  # NOQA
-from .accounts import *  # NOQA
-from .billing import *  # NOQA
-from .channels import *  # NOQA
-from .classes import *  # NOQA
-from .incentives import *  # NOQA
-from .traces import *  # NOQA
