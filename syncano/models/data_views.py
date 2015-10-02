77981f6307461ad1ed30ccf6b255ba831dca4568
Zhe Brah <zhebrah@gmail.com>
2015-10-02 19:32:13 +0200

    [SYNPYTH-160] add rename and get_api

--- syncano/models/data_views.py
+++ syncano/models/data_views.py
@@ -9,7 +9,7 @@ class DataView(Model):
 
     LINKS = [
         {'type': 'detail', 'name': 'self'},
-        {'type': 'list', 'name': 'objects'},
+        {'type': 'list', 'name': 'data_views'},
     ]
 
     PERMISSIONS_CHOICES = (
@@ -47,10 +47,24 @@ class DataView(Model):
             },
             'get_api': {
                 'methods': ['get'],
-                'path': '/api/objects/{name}/get_api',
+                'path': '/api/objects/{name}/get_api/',
             },
             'rename': {
                 'methods': ['post'],
-                'path': '/api/objects/{name}/rename',
+                'path': '/api/objects/{name}/rename/',
             }
         }
+
+    def rename(self, new_name):
+        properties = self.get_endpoint_data()
+        endpoint = self._meta.resolve_endpoint('rename', properties)
+        connection = self._get_connection()
+        return connection.request('POST',
+                                  endpoint,
+                                  data={'new_name': new_name})
+
+    def get_api(self):
+        properties = self.get_endpoint_data()
+        endpoint = self._meta.resolve_endpoint('get_api', properties)
+        connection = self._get_connection()
+        return connection.request('GET', endpoint)
