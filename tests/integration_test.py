e64b03e09e9db7866c71c58218995d692a58f702
Sebastian Opalczynski <opalczynski@gmail.com>
2015-11-12 14:35:12 +0100

    [LIB-201] restore old test code;

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -69,7 +69,7 @@ class InstanceIntegrationTest(IntegrationTest):
         name = 'i%s' % self.generate_hash()[:10]
         description = 'IntegrationTest'
 
-        self.assertEqual(len(list(self.model.please.list())), 1)  # auto create first instance;
+        self.assertFalse(bool(self.model.please.list()))
 
         instance = self.model.please.create(name=name, description=description)
         self.assertIsNotNone(instance.pk)
