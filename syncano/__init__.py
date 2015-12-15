8c24efd82bcc3a0c63857cb311eee25d7a9c7ddf
Zhe Brah <zhebrah@gmail.com>
2015-12-15 11:48:41 +0100

    [LIB-352] version bump

--- syncano/__init__.py
+++ syncano/__init__.py
@@ -2,7 +2,7 @@ import logging
 import os
 
 __title__ = 'Syncano Python'
-__version__ = '4.0.9'
+__version__ = '4.0.10'
 __author__ = "Daniel Kopka, Michal Kobus, and Sebastian Opalczynski"
 __credits__ = ["Daniel Kopka",
                "Michal Kobus",
