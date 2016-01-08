e703396ee4c1b6f96529954c4e8e04cb03dfa10d
Sebastian Opalczynski <opalczynski@gmail.com>
2016-01-08 16:15:21 +0100

    [LIB-421] add removed version to deprecation warning

--- syncano/release_utils.py
+++ syncano/release_utils.py
@@ -7,13 +7,17 @@ warnings.simplefilter('default')
 
 class Deprecated(object):
 
-    def __init__(self, lineno):
+    def __init__(self, lineno, removed_in_version):
         self.lineno = lineno  # how many decorators decorates the depracated func;
+        self.removed_in_version = removed_in_version
 
     def __call__(self, original_func):
         def new_func(*args, **kwargs):
             warnings.showwarning(
-                message="Call to deprecated function '{}'.".format(original_func.__name__),
+                message="Call to deprecated function '{}'. Will be removed in version: {}.".format(
+                    original_func.__name__,
+                    self.removed_in_version
+                ),
                 category=DeprecationWarning,
                 filename=original_func.func_code.co_filename,
                 lineno=original_func.func_code.co_firstlineno + self.lineno)
--- tests/test_deprecation_decorator.py
+++ tests/test_deprecation_decorator.py
@@ -11,16 +11,17 @@ class DeprecationDecoratorTestCase(unittest.TestCase):
 
         class SomeClass(object):
 
-            @Deprecated(lineno=0)
+            @Deprecated(lineno=0, removed_in_version='5.0.10')
             def some_deprecated_method(self):
                 pass
 
         with warnings.catch_warnings(record=True) as warning:
             # Cause all warnings to always be triggered.
-            warnings.simplefilter("always")
+            warnings.simplefilter('always')
             # Trigger a warning.
             SomeClass().some_deprecated_method()
             # Verify some things
             self.assertEqual(len(warning), 1)
             self.assertEqual(warning[-1].category, DeprecationWarning)
-            self.assertIn("deprecated", str(warning[-1].message))
+            self.assertIn('deprecated', str(warning[-1].message))
+            self.assertIn('5.0.10', str(warning[-1].message))
