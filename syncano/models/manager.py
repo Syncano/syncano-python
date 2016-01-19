2fb3d8b067fa0cab1c12999184447681a2028b03
Sebastian Opalczynski <opalczynski@gmail.com>
2016-01-19 14:35:07 +0100

    [LIB-311] rework count DataObject manager method;

--- syncano/models/manager.py
+++ syncano/models/manager.py
@@ -900,15 +900,20 @@ class ObjectManager(Manager):
             Object.please.list(instance_name='raptor', class_name='some_class').filter(id__gt=600).count()
             Object.please.list(instance_name='raptor', class_name='some_class').count()
             Object.please.all(instance_name='raptor', class_name='some_class').count()
-        :return: The integer with estimated objects count;
+        :return: Two elements tuple with objects list and count: objects, count = DataObjects.please.list(...).count();
         """
         self.method = 'GET'
         self.query.update({
             'include_count': True,
-            'page_size': 0,
+            'page_size': 20,
         })
         response = self.request()
-        return response['objects_count']
+        raw_objects = response['objects']
+        objects = []
+        for raw_data in raw_objects:
+            raw_data.update(self.properties)
+            objects.append(self.model(**raw_data))
+        return objects, response['objects_count']
 
     @clone
     def filter(self, **kwargs):
