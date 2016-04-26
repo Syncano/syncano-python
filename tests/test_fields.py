5d9ccf25126baeeb329581e1d4ef76c3b3c3d89f
Sebastian Opalczynski <opalczynski@gmail.com>
2016-04-26 14:21:23 +0200

    [LIB-653] correct field geo test;

--- tests/test_fields.py
+++ tests/test_fields.py
@@ -5,7 +5,6 @@ from functools import wraps
 from time import mktime
 
 import six
-from models import GeoPoint
 from syncano import models
 from syncano.exceptions import SyncanoValidationError, SyncanoValueError
 from syncano.models.manager import SchemaManager
@@ -583,7 +582,7 @@ class GeoPointTestCase(BaseTestCase):
         with self.assertRaises(SyncanoValueError):
             self.field.validate(12, self.instance)
 
-        self.field.validate(GeoPoint.GeoPointStruct(**{'latitude': 52.12, 'longitude': 12.02}), self.instance)
+        self.field.validate(models.GeoPoint.GeoPointStruct(**{'latitude': 52.12, 'longitude': 12.02}), self.instance)
 
     def test_to_python(self):
         with self.assertRaises(SyncanoValueError):
