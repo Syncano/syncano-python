bc93492f6cd1d816e32d00832455e144ab4bb64d
Sebastian Opałczyński <opalczynski@gmail.com>
2016-05-13 10:48:07 +0200

    [RELEASE v5.1.0] next release

--- syncano/__init__.py
+++ syncano/__init__.py
@@ -2,7 +2,7 @@ import logging
 import os
 
 __title__ = 'Syncano Python'
-__version__ = '5.0.2'
+__version__ = '5.1.0'
 __author__ = "Daniel Kopka, Michal Kobus, and Sebastian Opalczynski"
 __credits__ = ["Daniel Kopka",
                "Michal Kobus",
