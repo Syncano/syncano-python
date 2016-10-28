74c978c4f7f67d4a3410ee7be2333f9e820ca6a3
Sebastian Opałczyński <opalczynski@gmail.com>
2016-10-28 15:36:48 +0200

    [LIB-965] correct imports;

--- syncano/models/base.py
+++ syncano/models/base.py
@@ -11,7 +11,7 @@ from .traces import *  # NOQA
 from .push_notification import *  # NOQA
 from .geo import *  # NOQA
 from .backups import *  # NOQA
-from .hosting import *  # NOQA
+from .hosting import Hosting, HostingFile  # NOQA
 from .data_views import DataEndpoint as EndpointData  # NOQA
 from .custom_sockets import *  # NOQA
 from .custom_sockets_utils import Endpoint, ScriptCall, ScriptDependency, ClassDependency  # NOQA
